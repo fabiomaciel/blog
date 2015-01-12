9626070ceb2f3e7a2d05d109eeeb5a42a479aa00
Fabio Maciel <fabio.souza@callsp.inf.br>
2015-01-12 19:46:41 -0200

    add cv page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/curriculum', :to => redirect('/curriculum.html')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
