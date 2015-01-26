44052d5ef255aee527be3fe84deba5eaddeb242c
Fabio Maciel <fabio.souza@callsp.inf.br>
2015-01-26 18:26:18 -0200

    fix articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,32 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-    @article.save
-    redirect_to @article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if(@article.update(article_params))
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/curriculum', :to => redirect('/curriculum.html')
 
-  #get '/main', to: 'main#index'
-  #resources :articles
+  get '/main', to: 'main#index'
+  resources :articles
 
 
   # The priority is based upon order of creation: first created -> highest priority.
